be66251bdff5c43012dfe415c96881149f6297b9
yohrannes <yohrannes@gmail.com>
2023-06-24 08:08:06 -0300

    Reorganization
    Reorganizing and add more animations.

---
+++ js/anim-menu.js
@@ -0,0 +1,185 @@
+function clickSocial() {                                                            /* SOCIAL */
+if (menusocial.style.display == 'block') {
+menusocial.style.display = 'none'
+} else {
+menusocial.style.display = 'block'
+menuforum.style.display = 'none'
+menudevelop.style.display = 'none'
+menudesign.style.display = 'none'
+}
+
+if (menusocial.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'none'
+social1.style.display = 'block'
+social2.style.display = 'block'
+social3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+social1.style.display = 'none'
+social2.style.display = 'none'
+social3.style.display = 'none'
+}
+
+if (social1.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'none'
+social1.style.display = 'block'
+social2.style.display = 'block'
+social3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+social1.style.display = 'none'
+social2.style.display = 'none'
+social3.style.display = 'none'
+}
+
+}
+function clickForum() {                                                            /* FORUM */
+if (menuforum.style.display == 'block') {
+menuforum.style.display = 'none'
+} else {
+menusocial.style.display = 'none'
+menuforum.style.display = 'block'
+menudevelop.style.display = 'none'
+menudesign.style.display = 'none'
+}
+
+if (menuforum.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'block'
+develop.style.display = 'none'
+design.style.display = 'none'
+forum1.style.display = 'block'
+forum2.style.display = 'block'
+forum3.style.display = 'block'
+} else {
+social.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+forum1.style.display = 'none'
+forum2.style.display = 'none'
+forum3.style.display = 'none'
+}
+
+if (forum1.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'none'
+forum1.style.display = 'block'
+forum2.style.display = 'block'
+forum3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+forum1.style.display = 'none'
+forum2.style.display = 'none'
+forum3.style.display = 'none'
+}
+
+}
+function clickDevelop() {                                                            /* DEVELOP */
+if (menudevelop.style.display == 'block') {
+menudevelop.style.display = 'none'
+} else {
+menusocial.style.display = 'none'
+menuforum.style.display = 'none'
+menudevelop.style.display = 'block'
+menudesign.style.display = 'none'
+}
+
+if (menudevelop.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'block'
+design.style.display = 'none'
+develop1.style.display = 'block'
+develop2.style.display = 'block'
+develop3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+design.style.display = 'block'
+develop1.style.display = 'none'
+develop2.style.display = 'none'
+develop3.style.display = 'none'
+}
+
+if (develop1.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'none'
+develop1.style.display = 'block'
+develop2.style.display = 'block'
+develop3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+develop1.style.display = 'none'
+develop2.style.display = 'none'
+develop3.style.display = 'none'
+}
+
+}
+function clickDesign() {                                                            /* DESIGN */
+if (menudesign.style.display == 'block') {
+menudesign.style.display = 'none'
+} else {
+menusocial.style.display = 'none'
+menuforum.style.display = 'none'
+menudevelop.style.display = 'none'
+menudesign.style.display = 'block'
+}
+
+if (menudesign.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'block'
+design1.style.display = 'block'
+design2.style.display = 'block'
+design3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+design.style.display = 'block'
+design1.style.display = 'none'
+design2.style.display = 'none'
+design3.style.display = 'none'
+}
+
+if (design1.style.display == 'block') {
+social.style.display = 'none'
+forum.style.display = 'none'
+develop.style.display = 'none'
+design.style.display = 'none'
+design1.style.display = 'block'
+design2.style.display = 'block'
+design3.style.display = 'block'
+} else {
+social.style.display = 'block'
+forum.style.display = 'block'
+develop.style.display = 'block'
+design.style.display = 'block'
+design1.style.display = 'none'
+design2.style.display = 'none'
+design3.style.display = 'none'
+}
+
+}
---
+++ js/anim-titulo.js
@@ -0,0 +1,7 @@
+        function HoverShowTitle() {
+
+        }
+
+        function HoverHideTitle() {
+
+        }
