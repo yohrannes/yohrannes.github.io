7e453507bcec16991fce1994748b7661b592f976
yohrannes <yohrannes@gmail.com>
2023-06-26 07:42:58 -0300

    Ajusting title

--- js/anim-titulo.js
+++ js/anim-titulo.js
@@ -1,7 +1,11 @@
-        function HoverShowTitle() {
-
-        }
-
-        function HoverHideTitle() {
-
-        }
+function HoverShowTitle() {
+        document.getElementById("foto1").style.display = "block";
+        document.getElementById("foto2").style.display = "block";
+        document.getElementById("foto").style.display = "block";
+      }
+      
+      function HoverHideTitle() {
+        document.getElementById("foto1").style.display = "none";
+        document.getElementById("foto2").style.display = "none";
+        document.getElementById("foto").style.display = "none";
+      }
