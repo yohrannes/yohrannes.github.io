e402cc7da639613d43089258515307aafee39e63
yohrannes <yohrannes@gmail.com>
2023-06-27 18:20:27 -0300

    change typewrite tag id

--- js/anim-titulo.js
+++ js/anim-titulo.js
@@ -2,12 +2,12 @@ function HoverShowTitle() {
         document.getElementById("foto1").style.display = "block";
         document.getElementById("foto2").style.display = "block";
         document.getElementById("foto").style.display = "block";
-        document.getElementById("digitado").style.display = "block";
+        document.getElementById("typewrite").style.display = "block";
       }
       
       function HoverHideTitle() {
         document.getElementById("foto1").style.display = "none";
         document.getElementById("foto2").style.display = "none";
         document.getElementById("foto").style.display = "none";
-        document.getElementById("digitado").style.display = "none";
+        document.getElementById("typewrite").style.display = "none";
       }
