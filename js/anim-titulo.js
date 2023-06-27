cf5b6b2327e1259c30fcf7331b0aafe28dc06cc3
yohrannes <yohrannes@gmail.com>
2023-06-27 18:19:11 -0300

    add new animation

--- js/anim-titulo.js
+++ js/anim-titulo.js
@@ -2,10 +2,12 @@ function HoverShowTitle() {
         document.getElementById("foto1").style.display = "block";
         document.getElementById("foto2").style.display = "block";
         document.getElementById("foto").style.display = "block";
+        document.getElementById("digitado").style.display = "block";
       }
       
       function HoverHideTitle() {
         document.getElementById("foto1").style.display = "none";
         document.getElementById("foto2").style.display = "none";
         document.getElementById("foto").style.display = "none";
+        document.getElementById("digitado").style.display = "none";
       }
